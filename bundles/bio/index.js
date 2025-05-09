b87cba8091f2d5f54bda2ad401680bca7ec667ba
marecnov <73169648+marecnov@users.noreply.github.com>
2025-05-09 13:19:15 +0300

    bio indexJS

---
+++ bundles/bio/index.js
@@ -0,0 +1,28 @@
+export default [
+`
+    <div class="about-me">
+        <div class='openCloser'>
+            <p>About me</p>
+            <i class="fa fa-chevron-down" aria-hidden="true"></i>
+        </div>
+        <div class="panel">
+            <img src="/static/avatar.jpg">
+            <h2>Marek Ivanov</h2>
+            <p>I do live life, enjoy great things, create meaning...</p>
+        </div>
+    </div>
+`,
+`
+    <div class="about-me">
+        <div class='openCloser'>
+            <p>Обо мне</p>
+            <i class="fa fa-chevron-down" aria-hidden="true"></i>
+        </div>
+        <div class="panel">
+            <img src="/static/avatar.jpg">
+            <h2>Марек Иванов</h2>
+            <p>Живу жизнью, наслаждаюсь великим, создаю смыслы...</p>
+        </div> 
+    </div>
+`
+]
