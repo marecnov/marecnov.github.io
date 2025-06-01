a1d3eda13338613f0bfd99062645458490384c0f
marecnov <73169648+marecnov@users.noreply.github.com>
2025-06-01 17:36:50 +0300

    Update index.js

--- bundles/settings/index.js
+++ bundles/settings/index.js
@@ -1,52 +1,46 @@
 export default [
 `
-<a id="settingsLink" style="cursor: pointer;"><i class="fa fa-cog" aria-hidden="true"></i></a>
-<div id="myModal" class="modal">
-    <div class="modal-content">
-        <div class="modal-header">
-          <h2>Settings</h2>
-          <span class="close">&times;</span>
-        </div>
-        <div class="modal-body">
-            <section><div><i class="fa fa-globe" aria-hidden="true"></i> Language</div>
-                <select>
-                    <option value="en">English</option>
-                    <option value="ru">Russian</option>
-                </select>
-            </section>
-            <section><div><i class="fa fa-moon-o" aria-hidden="true"></i> Dark mode</div>
-                <input type="checkbox">
-            </section>
-        </div>
-        <div class="modal-footer">
-            <button>Apply</button>
-        </div>
-    </div>
+<a id="settingsLink">
+<i class="fa fa-cog" aria-hidden="true"></i></a>
+<div id="modal-overlay">
+<div id="modal">
+  <h2>Settings</h2>
+  <a class="close modal-r">&times;</a>
+        <section>
+            <i class="fa fa-globe" aria-hidden="true"></i> Language
+        </section>
+        <select class="modal-r">
+            <option value="en">English</option>
+            <option value="ru">Russian</option>
+        </select>
+        <section>
+            <i class="fa fa-moon-o" aria-hidden="true"></i> Dark mode
+        </section>
+        <input type="checkbox" class="modal-r">
+  <button class="modal-r">Apply</button>
+</div>
 </div>
 `,
 `
-<a id="settingsLink" style="cursor: pointer;"><i class="fa fa-cog" aria-hidden="true"></i></a>
-  <div id="myModal" class="modal">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h2>Настройки</h2>
-          <span class="close">&times;</span>
-        </div>
-        <div class="modal-body">
-            <section><div><i class="fa fa-globe" aria-hidden="true"></i> Язык</div>
-                <select>
-                    <option value="en">Английский</option>
-                    <option value="ru">Русский</option>
-                </select>
-            </section>
-            <section><div><i class="fa fa-moon-o" aria-hidden="true"></i> Темный режим</div>
-                <input type="checkbox">
-            </section>
-        </div>
-        <div class="modal-footer">
-            <button>Применить</button>
-        </div>
-      </div>
-  </div>
+<a id="settingsLink">
+<i class="fa fa-cog" aria-hidden="true"></i></a>
+<div id="modal-overlay">
+<div id="modal">
+  <h2>Настройки</h2>
+  <a class="close modal-r">&times;</a>
+        <section>
+            <i class="fa fa-globe" aria-hidden="true"></i> Язык
+        </section>
+        <select class="modal-r">
+            <option value="en">Английский</option>
+            <option value="ru">Русский</option>
+        </select>
+        <section>
+            <i class="fa fa-moon-o" aria-hidden="true"></i> Темный режим
+        </section>
+        <input type="checkbox" class="modal-r">
+  <button class="modal-r">Применить</button>
+</div>
+</div>
 `
 ]
