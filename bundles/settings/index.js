f3346f62307a65bfc3f5085872471f6969d18952
marecnov <73169648+marecnov@users.noreply.github.com>
2026-08-30 07:12:31 +0300

    Update index.js

--- bundles/settings/index.js
+++ bundles/settings/index.js
@@ -1,46 +1,5 @@
-export default [
+export default 
 `
-<a id="settingsLink">
-<i class="fa fa-cog" aria-hidden="true"></i></a>
-<div id="modal-overlay">
-<div id="modal">
-  <h2>Settings</h2>
-  <a class="close modal-r">&times;</a>
-        <section>
-            <i class="fa fa-globe" aria-hidden="true"></i> Language
-        </section>
-        <select class="modal-r">
-            <option value="en">English</option>
-            <option value="ru">Russian</option>
-        </select>
-        <section>
-            <i class="fa fa-moon-o" aria-hidden="true"></i> Dark mode
-        </section>
-        <input type="checkbox" class="modal-r">
-  <button class="modal-r">Apply</button>
-</div>
-</div>
-`,
+<a id="darkMode">
+<i class="fa fa-moon-o" aria-hidden="true"></i></a>
 `
-<a id="settingsLink">
-<i class="fa fa-cog" aria-hidden="true"></i></a>
-<div id="modal-overlay">
-<div id="modal">
-  <h2>Настройки</h2>
-  <a class="close modal-r">&times;</a>
-        <section>
-            <i class="fa fa-globe" aria-hidden="true"></i> Язык
-        </section>
-        <select class="modal-r">
-            <option value="en">Английский</option>
-            <option value="ru">Русский</option>
-        </select>
-        <section>
-            <i class="fa fa-moon-o" aria-hidden="true"></i> Темный режим
-        </section>
-        <input type="checkbox" class="modal-r">
-  <button class="modal-r">Применить</button>
-</div>
-</div>
-`
-]
